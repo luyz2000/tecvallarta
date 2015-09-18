432a359cc0c14dafe25b0869bb9a1f65b3f0b3aa
KaiserFrederick <codefluid@hotmail.com>
2015-09-18 16:52:18 -0500

    Avance de Perfiles
    Los amo eeeee 24

---
+++ Agea/app/src/main/java/com/example/agea/FrmPerfil.java
@@ -0,0 +1,14 @@
+package com.example.agea;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class FrmPerfil extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_perfil);
+
+    }
+}
---
+++ Agea/app/src/main/java/com/example/agea/PerfilVO.java
@@ -0,0 +1,31 @@
+package com.example.agea;
+
+public class PerfilVO {
+    private int perId;
+    private String perNombre;
+    private String perPermiso;
+
+    public int getPerId() {
+        return perId;
+    }
+
+    public void setPerId(int perId) {
+        this.perId = perId;
+    }
+
+    public String getPerNombre() {
+        return perNombre;
+    }
+
+    public void setPerNombre(String perNombre) {
+        this.perNombre = perNombre;
+    }
+
+    public String getPerPermiso() {
+        return perPermiso;
+    }
+
+    public void setPerPermiso(String perPermiso) {
+        this.perPermiso = perPermiso;
+    }
+}
