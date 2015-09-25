6aec0f1102fd8fc7aef8ff4e3c8c9a09e3cb6aac
KaiserFrederick <codefluid@hotmail.com>
2015-09-25 15:48:49 -0500

    Valores nuevos

--- Agea/app/src/main/java/com/example/agea/FrmPerfil.java
+++ Agea/app/src/main/java/com/example/agea/FrmPerfil.java
@@ -1,11 +1,14 @@
 package com.example.agea;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class FrmPerfil extends ActionBarActivity {
 
@@ -29,8 +32,29 @@ public class FrmPerfil extends ActionBarActivity {
             case R.id.buscar_registro_perfil:
 
                 return true;
+            case R.id.nuevo_registro_perfil:
 
-
+                return true;
+            case R.id.eliminar_registro_perfil:
+                return true;
+            case R.id.salir_registro_perfil:
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                alertDialog.setTitle("¿DESEAS SALIR DE LA APP AGEA?");
+                alertDialog.setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.exit(0);
+                    }
+                });
+                alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "CONTINUA", Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog dialog = alertDialog.create();
+                dialog.show();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
