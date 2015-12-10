08b38dd2b6aa76401ea8cd4265cdee557353f349
Kai <codefluid@hotmail.com>
2015-12-10 11:26:32 -0600

    JAjajaja

---
+++ Agea/app/src/main/java/com/example/agea/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.agea;
+
+/**
+ * Created by Light on 10/12/2015.
+ */
+public class AppConfig {
+    public static String URL_LOGIN="http://ageaitspv.netau.net/login.php";
+
+    public static String URL_REGISTER="http://ageaitspv.netau.net/register.php";
+
+    public static String URL_PAY="http://ageaitspv.netau.net/register.php";
+}
---
+++ Agea/app/src/main/java/com/example/agea/AppController.java
@@ -0,0 +1,9 @@
+package com.example.agea;
+
+/**
+ * Created by Light on 10/12/2015.
+ */
+public class AppController {
+
+
+}
